8cd1cf88456ab2b3da2747537c8b6843a59edbd2
Dax Raad <d@ironbay.co>
2024-09-05 15:48:48 -0400

    sync

--- examples/aws-hono/sst.config.ts
+++ examples/aws-hono/sst.config.ts
@@ -16,6 +16,9 @@ export default $config({
       url: true,
       link: [bucket],
       handler: "index.handler",
+      nodejs: {
+        plugins: "./plugins.mjs",
+      },
     });
     return {
       api: hono.url,
